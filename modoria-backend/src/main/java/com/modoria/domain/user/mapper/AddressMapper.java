e43810a8029aa915c240f8fa8898c7e35a6644c3
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2026-02-24 11:32:53 +0000

    feat(user): add user mappers for DTO conversion

---
+++ modoria-backend/src/main/java/com/modoria/domain/user/mapper/AddressMapper.java
@@ -0,0 +1,32 @@
+package com.modoria.domain.user.mapper;
+
+import com.modoria.domain.user.dto.request.AddressRequest;
+import com.modoria.domain.user.dto.response.AddressResponse;
+import com.modoria.domain.user.entity.Address;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface AddressMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    Address toEntity(AddressRequest request);
+
+    AddressResponse toResponse(Address address);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    void updateEntityFromRequest(AddressRequest request, @MappingTarget Address address);
+}
